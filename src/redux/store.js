73a7c0f8836b53035444c07b6d0a499e3e826a1f
baoPQ258 <150884103+baoPQ258@users.noreply.github.com>
2024-02-01 09:30:06 +0700

    fix bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,15 @@ import createSagaMiddleware from "redux-saga";
 import rootReducer from "./roootReducers";
 import rootSagas from "./rootSagas";
 
+
+
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSagas);
+
+
+
 
 export default store;
